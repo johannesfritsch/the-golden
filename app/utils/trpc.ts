b686cb3f9ca0cb2c67ba94a066cef5c10b1deb72
Johannes Fritsch <fritsch@zoom7.de>
2024-09-18 19:35:51 +0200

    Probably fixed the issues with monorepo and expo-router

--- app/utils/trpc.ts
+++ app/utils/trpc.ts
@@ -1,4 +1,4 @@
 import { createTRPCReact } from '@trpc/react-query';
-import type { AppRouter } from '<@the-golden-foundation/api'
+import type { AppRouter } from '@the-golden-foundation/api'
  
 export const trpc = createTRPCReact<AppRouter>();
