8ab600bdbc7683ede6faf0b6ca2cfc07385a3f06
Johannes Fritsch <fritsch@zoom7.de>
2024-09-26 12:15:17 +0200

    Reworked onboarding

--- app/app/(app)/onboarding/index.tsx
+++ app/app/(app)/onboarding/index.tsx
@@ -2,11 +2,12 @@ import Button from '@/components/Button'
 import CText from '@/components/CText'
 import PageControl from '@/components/PageControl'
 import { router } from 'expo-router'
-import { useMemo, useRef, useState } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
 import { Dimensions, PanResponder, Pressable, View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import ContentSlider from '@/components/ContentSlider'
 import VideoPlayer from '@/components/VideoPlayer'
+import Animated, { Easing, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated'
 
 const videos = [
     require('@/assets/videos/closeup-arm-wrist.mp4'),
@@ -20,6 +21,35 @@ const Onboarding = () => {
     const insets = useSafeAreaInsets();
     const [currentSlide, setCurrentSlide] = useState(0);
     const [handled, setHandled] = useState(false);
+
+    const contentHeight = useSharedValue(30.0);
+
+    const videoPlayerStyle = useAnimatedStyle(() => ({ 
+        height: `${100 - contentHeight.value}%`, 
+        backgroundColor: '#000', 
+        marginBottom: -30 
+    }));
+
+    const contentStyle = useAnimatedStyle(() => ({
+        height: `${contentHeight.value}%`,
+        backgroundColor: 'white',
+        paddingTop: 25,
+        paddingHorizontal: 30,
+        borderTopEndRadius: 30,
+        borderTopStartRadius: 30,
+        paddingBottom: insets.bottom,
+    }));
+
+    const pageControlStyle = useAnimatedStyle(() => ({
+        marginTop: 40 - contentHeight.value,
+    }));
+
+    useEffect(() => {
+        contentHeight.value = withTiming([35.0, 35.0, 35.0, 35.0, 50.0][currentSlide], {
+            duration: 500,
+            easing: Easing.elastic(1.5),
+        });
+    }, [currentSlide]);
 
     const panResponder = useMemo(() => {
         return PanResponder.create({
@@ -42,59 +72,42 @@ const Onboarding = () => {
 
     return (
         <View {...panResponder.panHandlers} style={{ width: '100%', height: '100%', backgroundColor: 'white' }}>
-            <View style={{ height: '50%', backgroundColor: '#000', marginBottom: -30 }}>
+            <Animated.View style={videoPlayerStyle}>
                 <VideoPlayer source={videos[currentSlide]} />
-            </View>
-            <View style={{
-                height: '50%',
-                backgroundColor: 'white',
-                paddingTop: 25,
-                paddingBottom: insets.bottom,
-                paddingHorizontal: 30,
-                borderTopEndRadius: 30,
-                borderTopStartRadius: 30,
-            }}>
+            </Animated.View>
+            <Animated.View style={contentStyle}>
                 <View style={{ flexDirection: 'column', justifyContent: 'center', gap: 20 }}>
-                    <View style={{}}>
+                    <Animated.View style={pageControlStyle}>
                         <PageControl currentPage={currentSlide} totalPages={5} onPagePress={(index) => {
                             setCurrentSlide(index);
                         }} />
-                    </View>
+                    </Animated.View>
                     <View style={{}}>
                         <ContentSlider currentSlide={currentSlide} extraGap={30} width={Dimensions.get('window').width - (2 * 30)}>
                             <ContentSlider.Item>
                                 <CText type='h1' style={{ textAlign: 'center', marginBottom: 10 }}>Welcome to The Golden</CText>
-                                <CText type='normal' style={{ textAlign: 'center', marginBottom: 40 }}>The Golden is an exclusive community of like-minded people. We offer best-in-class events in very exclusive venues for people to meet, relax and escape the ordinary.</CText>
-                                <Button style={{ marginBottom: 30, paddingHorizontal: 50 }} chevron caption='' onClick={() => {
-                                    setCurrentSlide(1);
-                                }} />
-                                <Pressable onPress={() => setCurrentSlide(4)}><CText type='bold' style={{ textAlign: 'center' }}>Skip introduction</CText></Pressable>
+                                <CText type='normal' style={{ textAlign: 'center', marginBottom: 20 }}>The Golden is an exclusive community of like-minded people. We offer best-in-class events in very exclusive venues for people to meet, relax and escape the ordinary.</CText>
+                                <CText type='bold' style={{ textAlign: 'center', opacity: 0.3 }}>Swipe right to proceed</CText>
                             </ContentSlider.Item>
                             <ContentSlider.Item>
                                 <CText type='h1' style={{ textAlign: 'center', marginBottom: 10 }}>Extraordinary Venues</CText>
-                                <CText type='normal' style={{ textAlign: 'center', marginBottom: 40 }}>The Golden takes you to extraordinary venues that go beyond the expected—exclusive locations chosen for their beauty, uniqueness, and rarity.</CText>
-                                <Button chevron caption='' style={{ paddingHorizontal: 50 }} onClick={() => {
-                                    setCurrentSlide(2);
-                                }} />
+                                <CText type='normal' style={{ textAlign: 'center', marginBottom: 20 }}>The Golden takes you to extraordinary venues that go beyond the expected—exclusive locations chosen for their beauty, uniqueness, and rarity.</CText>
+                                <CText type='bold' style={{ textAlign: 'center', opacity: 0.3 }}>Swipe right to proceed</CText>
                             </ContentSlider.Item>
                             <ContentSlider.Item>
                                 <CText type='h1' style={{ textAlign: 'center', marginBottom: 10 }}>Curated Elegance</CText>
-                                <CText type='normal' style={{ textAlign: 'center', marginBottom: 40 }}>Each location is transformed with thoughtful details into an effortless yet unforgettable memory. On top of that, our events come with a well thought-of schedule of exclusive activites.</CText>
-                                <Button chevron caption='' style={{ paddingHorizontal: 50 }} onClick={() => {
-                                    setCurrentSlide(3);
-                                }} />
+                                <CText type='normal' style={{ textAlign: 'center', marginBottom: 20 }}>Each location is transformed with thoughtful details into an effortless yet unforgettable memory. On top of that, our events come with a well thought-of schedule of exclusive activites.</CText>
+                                <CText type='bold' style={{ textAlign: 'center', opacity: 0.3 }}>Swipe right to proceed</CText>
                             </ContentSlider.Item>
                             <ContentSlider.Item>
                                 <CText type='h1' style={{ textAlign: 'center', marginBottom: 20 }}>Most Exclusive Guest Lists</CText>
-                                <CText type='normal' style={{ textAlign: 'center', marginBottom: 40 }}>At The Golden, our guest lists are peer-reviewed, ensuring every event brings together a remarkable circle of influential individuals.</CText>
-                                <Button chevron style={{ marginBottom: 20, paddingHorizontal: 50 }} caption="" onClick={() => {
-                                    setCurrentSlide(4);
-                                }} />
+                                <CText type='normal' style={{ textAlign: 'center', marginBottom: 20 }}>At The Golden, our guest lists are peer-reviewed, ensuring every event brings together a remarkable circle of influential individuals.</CText>
+                                <CText type='bold' style={{ textAlign: 'center', opacity: 0.3 }}>Swipe right to proceed</CText>
                             </ContentSlider.Item>
                             <ContentSlider.Item>
                                 <CText type='h1' style={{ textAlign: 'center', marginBottom: 20 }}>Already got an Aura?</CText>
-                                <CText type='normal' style={{ textAlign: 'center', marginBottom: 40 }}>Auras are your key to the world of The Golden's exclusive events. They are exquisite jewelry pieces, seamlessly integrated with ultra-secure NFC technology.</CText>
-                                <Button style={{ marginBottom: 30 }} caption="Get your own Aura" onClick={() => {
+                                <CText type='normal' style={{ textAlign: 'center', marginBottom: 40 }}>Auras are keys to the world of The Golden's exclusive events. They are exquisite jewelry pieces, seamlessly integrated with ultra-secure NFC technology.</CText>
+                                <Button style={{ marginBottom: 30 }} caption="See buying options" onClick={() => {
                                     router.navigate('/waitlist');
                                 }} />
                                 <Pressable onPress={() => router.navigate('/events')}><CText type='bold' style={{ textAlign: 'center' }}>Already got an Aura</CText></Pressable>
@@ -102,7 +115,7 @@ const Onboarding = () => {
                         </ContentSlider>
                     </View>
                 </View>
-            </View>
+            </Animated.View>
         </View>
 
     )
