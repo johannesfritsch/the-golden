efb2a0b7467affe282a969e3d362002c5947e258
Johannes Fritsch <fritsch@zoom7.de>
2025-10-20 06:11:57 +0200

    Added a page for NFC

---
+++ web/src/app/nfc/page.tsx
@@ -0,0 +1,10 @@
+export default function NFCPage() {
+  return (
+    <main style={{ display: 'flex', minHeight: '60vh', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', gap: 8 }}>
+      <h1>/nfc</h1>
+      <p>This is a minimal sample page for NFC.</p>
+    </main>
+  );
+}
+
+
